f268d2620b100995be38a6281529ef7583993525
doan van tuan <doantuanvan2003@gmail.com>
2023-11-11 22:32:56 +0700

    #8 - add welcome screen

---
+++ foodfresh/src/navigators/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {SplashScreen} from '../screens';
+import { Screen } from 'react-native-screens';
+import WelcomeScreen from '../screens/WelcomeScreen';
+
+
+const Stack = createStackNavigator();
+
+const Navigators = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='WelcomeScreen' component={WelcomeScreen}/>
+        <Stack.Screen name='Splash' component={SplashScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigators;

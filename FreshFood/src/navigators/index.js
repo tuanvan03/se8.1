532f3f03fe6833612702045985501c46f14c3992
NamPH03 <phamhoainam03@gmail.com>
2023-11-11 17:47:09 +0700

    #13 -Them SplashScreen

---
+++ FreshFood/src/navigators/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {SplashScreen} from '../screens';
+import { Screen } from 'react-native-screens';
+
+
+const Stack = createStackNavigator();
+
+const Navigators = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Splash' component={SplashScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigators;

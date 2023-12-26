bef22330d733dab7400c6479e7cacfe148f42c53
tuanvan03 <doantuanvan2003@gmail.com>
2023-12-26 15:08:02 +0700

    #32 - API Integration SignUp and SignIn

--- FreshFood/src/services/AuthenticationService.js
+++ FreshFood/src/services/AuthenticationService.js
@@ -26,4 +26,38 @@ const register = async user => {
     }
   };
 
-  export default {register};
+  const login = async user => {
+    if (!user?.username || !user?.password) {
+      return {status: false, message: 'Please fill up all fields'};
+    }
+    try {
+      let requestBody = {
+        username: user?.username,
+        password: user?.password,
+      };
+      let loginResponse = await AuthRequest.post(
+        ApiContants.BACKEND_API.LOGIN,
+        requestBody,
+      );
+      return loginResponse?.data;
+    } catch (error) {
+      console.log(error);
+      return {status: false, message: 'Oops! Something went wrong'};
+    }
+  };
+  
+  const checkUserExist = async (type, value) => {
+    try {
+      let params = {[type]: value};
+      let userCheckResponse = await AuthRequest.get(
+        ApiContants.BACKEND_API.USER_EXIST,
+        {params},
+      );
+      return userCheckResponse?.data;
+    } catch (error) {
+      console.log(error);
+      return {status: false, message: 'Ầu nâu!!! Săm thing lỗi'};
+    }
+  };
+
+  export default {register, checkUserExist, login};
